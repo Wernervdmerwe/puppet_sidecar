19ed0253b51516a616b76bb6de420812dcfbc0b7
Werner vd Merwe <wernervdmerwe@gmail.com>
2016-09-06 23:26:07 +1200

    Fix file creation dependancy

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,15 +7,16 @@ class sidecar (
     ensure => 'installed',
   }
 
-  exec {'Create File If Not Exist':
-    path => $path,
-    command => "echo $::kernel > $tagfile",
-    creates => $tagfile,
-  }
+  #exec {'Create File If Not Exist':
+  #  path => $path,
+  #  command => "echo $::kernel > $tagfile",
+  #  creates => $tagfile,
+  #}
 
+  include sidecar::file
   include sidecar::tags
   include sidecar::config
   include sidecar::service
 
-  Class['sidecar::tags'] -> Class['sidecar::config']
+  Class['sidecar::file'] -> Class['sidecar::tags'] -> Class['sidecar::config']
 }
