7bda80f35469a2b5bb0618f73526ef3662ee5d58
Werner vd Merwe <wernervdmerwe@gmail.com>
2016-09-06 20:29:34 +1200

    GL update

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,12 @@ class sidecar (
     ensure => 'installed',
   }
 
+  file {$tagfile:
+    ensure => 'file',
+    content => "$::kernel",
+    onlyif => "test ! -f $tagfile"
+  }
+
   include sidecar::tags
   include sidecar::config
   include sidecar::service
